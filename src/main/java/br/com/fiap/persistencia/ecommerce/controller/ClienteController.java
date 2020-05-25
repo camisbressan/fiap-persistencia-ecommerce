75005bfcc56e28a0d4073e82e42df0bca8c8bbbb
Camila Bressan <camila-bressan@hotmail.com>
2020-05-25 18:13:51 -0300

    Ajustando API de Login para Post

--- src/main/java/br/com/fiap/persistencia/ecommerce/controller/ClienteController.java
+++ src/main/java/br/com/fiap/persistencia/ecommerce/controller/ClienteController.java
@@ -68,7 +68,7 @@ public class ClienteController {
 		clienteService.delete(id);
 	}
 
-	@GetMapping("login")
+	@PostMapping("login")
 	public ResponseEntity<ClienteDTO> login(@RequestBody LoginDTO loginDTO) {
 		return ResponseEntity.ok(clienteService.login(loginDTO));
 	}
