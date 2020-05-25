ccbab10e2dcd2353ad3690994517c973e965b7a4
Ana <carolldsk@gmail.com>
2020-05-25 18:48:25 -0300

    tela de pedidos layout

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,12 +2,17 @@ import React from 'react';
 
 import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
 
-import Login from './views/Login';
+import Login   from './views/Login';
+
+import Pedidos from './views/Pedidos';
+
 
 const Routes = () =>(
     <BrowserRouter>
         <Switch>
             <Route exact path="/" component={() =><Login />} />
+            <Route exact path="/produtos" component={() =><h1>Produtos</h1>} />
+            <Route exact path="/pedidos" component={() =><Pedidos />} />
         </Switch>
     </BrowserRouter>
 );
