403860e8b49a08adcc463d2766821b6684933060
joliveira2012 <jaquelinesilvaoliveira2012@gmail.com>
2020-05-27 18:34:40 -0300

    criando tela carrinho de compras

--- client/src/routes.js
+++ client/src/routes.js
@@ -8,8 +8,8 @@ import Pedidos from './views/Pedidos';
 const Routes = () =>(
     <BrowserRouter>
         <Switch>
-            <Route exact path="/" component={() =><Login />} />
-            <Route exact path="/produtos" component={() =><Products />} />
+            <Route exact path="/produtos" component={() =><Login />} />
+            <Route exact path="/" component={() =><Products />} />
             <Route exact path="/pedidos" component={() =><Pedidos />} />
         </Switch>
     </BrowserRouter>
--- client/src/views/Products.js
+++ client/src/views/Products.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import withoutProduct from '../img/product-without-photo.svg';
-import carr from '../img/carr.svg';
+import car from '../img/carr.svg';
 import './General.css';
 import './Products.css'
 
@@ -12,6 +12,7 @@ class App extends Component {
             result: "",
             selectedProducts: 0
         };
+        this.productsOnCar = [];
     };
 
     componentWillMount() {
@@ -23,7 +24,7 @@ class App extends Component {
         }).then((response) => response.json()
             .then((data) => {
                 console.log(data)
-                this.setState({ products: data, result: "" })
+                this.setState({ products: data })
             })
         ).catch(
             error => {
@@ -34,35 +35,94 @@ class App extends Component {
 
     }
 
-    putOnCar = (idx, e) => {
+    putOnCar = (idx, product) => {
         let { products, selectedProducts } = this.state
 
-        let result = ""
-        //deu certo
-        console.log(products[idx].quantidade)
-        if (products[idx].quantidade > 0) {
-            products[idx].quantidade = products[idx].quantidade - 1;
+        if (product.quantidade > 0) {
+            products[idx].quantidade = product.quantidade - 1;
             selectedProducts = selectedProducts + 1;
-        } else {
-            result = "Produto em falta"
+
+            let idxExists = this.productsOnCar.findIndex((item) => { if (item.id === product.id) { return true } })
+
+            if (idxExists >= 0) {
+                this.productsOnCar[idxExists].quantidade = this.productsOnCar[idxExists].quantidade + 1;
+            } else {
+                this.productsOnCar[this.productsOnCar.length] = {
+                    id: product.id,
+                    nome: product.nome,
+                    descricao: product.descricao,
+                    quantidade: 1,
+                    preco: product.preco
+                }
+            }
+
+            this.setState({ products, selectedProducts, result: "" })
         }
-        console.log(products[idx].quantidade)
-        this.setState({ products, selectedProducts })
+    }
+
+    openCar = (idx, product) => {
+        let { result } = this.state
+
+        if (this.productsOnCar.length > 0) {
+            let vlTotal = this.productsOnCar.reduce(function (accumulator, currentValue) {
+                return accumulator + (currentValue.preco * currentValue.quantidade);
+            }, 0)
+
+            let productsSelected = [];
+            this.productsOnCar.forEach((prod, index) => {
+                productsSelected[index] = {
+                    produto: prod,
+                    quantidade: prod.quantidade
+                }
+            })
+
+            let body = JSON.stringify({
+                valorTotal: vlTotal,
+                itens: productsSelected
+            });
+
+
+            //passar o id do cliente
+            fetch("http://localhost:8080/v1/pedidos/cliente/1", {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: body,
+            }).then(function (response) {
+                if(response.ok){
+                    //chamar outra tela
+                } else {
+                    result = "Falha ao consultar o carrinho!";
+                    this.setState({ result })
+                }
+            }).catch(
+                error => {
+                    console.log(error)
+                }
+            );
+        } else {
+            result = "O carrinho ainda não possui nenhum produto!";
+            this.setState({ result })
+        }
 
     }
 
     render() {
-        let { products, selectedProducts } = this.state
+        let { products, result, selectedProducts } = this.state
         return (
             <div className="App">
                 <header className="App-header">
-                    <div className="w100 flex-space-between mr5 ml5 mt3">
+                    <div className="w90 flex-space-between mr5 ml5 mt3">
                         <h1 className="text-green">Produtos disponíveis</h1>
-                        <div>
-                            <img className="flex-end car mt1" src={carr} alt="logo-product" height="40px" width="40px" />
+                        <div className="clickable" onClick={this.openCar}>
+                            <img className="flex-end car mt1" src={car} alt="logo-product" height="40px" width="40px" />
                             <label className="text-green"><bold>{selectedProducts}</bold></label>
                         </div>
                     </div>
+
+                    <label className="error"><b>{result}</b></label>
                     <div className="products flex-center">
                         {products.map((product, index) => {
                             return (
@@ -76,10 +136,11 @@ class App extends Component {
                                         <label><b>Quantidade:</b> {product.quantidade}</label>
                                         <label><b>R$:</b> {product.preco}</label>
                                     </div>
-
-                                    <div className="mt5 flex-end">
-                                        <button onClick={event => this.putOnCar(index, event)}>COMPRAR</button>
-                                    </div>
+                                    {product.quantidade > 0 ?
+                                        <div className="mt5">
+                                            <button onClick={event => this.putOnCar(index, product)}>COMPRAR</button>
+                                        </div>
+                                        : null}
                                 </div>
                             );
                         })}
